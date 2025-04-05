7745018571d8f4feee788840bc78efd03ed4ccbf
MachaVivek <machavivek19@gmail.com>
2025-04-05 17:50:18 +0530

    added learn spring boot project

---
+++ learn/src/main/java/com/vivek/learn/hello.java
@@ -0,0 +1,13 @@
+package com.vivek.learn;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+    @RequestMapping
+    public String helloFunction(){
+        return "Hello world";
+    }
+}
+
---
+++ learnSpring/src/main/java/com/vivek/learnSpring/Hello.java
@@ -0,0 +1,4 @@
+package com.vivek.learnSpring;
+
+public class Hello {
+}
